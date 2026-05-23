d768b5cd4a9553bd2b38b4df9c7664cafb9f115e
‍ <63112335+CHNgsz@users.noreply.github.com>
2026-05-24 00:39:31 +0800

    Update function.py
    修改获取数据延迟

--- src/custom/function.py
+++ src/custom/function.py
@@ -10,7 +10,7 @@ if TYPE_CHECKING:
 
 
 def get_wait_time(
-    avg_delay: float | int = 6.0,
+    avg_delay: float | int = 3.0,
     sigma: float = 0.6,
 ) -> float:
     mu = log(avg_delay) - (sigma**2 / 2)
