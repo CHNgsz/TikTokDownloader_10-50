7f5204387edba3e1e2757b2ae048c9eb1e89917f
‍ <63112335+CHNgsz@users.noreply.github.com>
2026-09-14 19:44:51 +0800

    Update function.py

--- src/custom/function.py
+++ src/custom/function.py
@@ -14,7 +14,7 @@ def get_wait_time(
     sigma: float = 0.5,
 ) -> float:
     mu = log(avg_delay) - (sigma**2 / 2)
-    return min(3.0。， max(1.0， lognormvariate(mu, sigma)))
+    return min(3.0, max(1.0, lognormvariate(mu, sigma)))
 
 
 async def wait(
@@ -62,9 +62,9 @@ async def suspend(count: int, console: "ColorfulConsole") -> None:
     说明: 此处的一个数据代表一个账号或者一个合集，并非代表一个数据包
     """
     # 启用该函数
-    batches = 50  # 根据实际需求修改
+    batches = 50 # 根据实际需求修改
     if not count % batches:
-        rest_time = 60 * 3  # 根据实际需求修改
+        rest_time = 60 * 3 # 根据实际需求修改
         console.print(
             _(
                 "程序连续处理了 {batches} 个数据，为了避免请求频率过高导致账号或 IP 被风控，"
