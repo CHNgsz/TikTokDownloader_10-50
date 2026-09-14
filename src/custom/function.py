c86097fb4af3d1206af0fe0164c855e461e6e2ad
‍ <63112335+CHNgsz@users.noreply.github.com>
2026-09-14 19:48:07 +0800

    Update function.py

--- src/custom/function.py
+++ src/custom/function.py
@@ -10,7 +10,7 @@ if TYPE_CHECKING:
 
 
 def get_wait_time(
-    avg_delay: float | int = 1.5，
+    avg_delay: float | int = 1.5,
     sigma: float = 0.5,
 ) -> float:
     mu = log(avg_delay) - (sigma**2 / 2)
